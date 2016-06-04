0f8b36bf89ebe8771464d531b078abb4e89a32f8
Ciro Santilli <ciro.santilli@gmail.com>
2016-06-04 11:41:34 +0200

    Factor out upvote downvote signs, prepare for js update tag upvotes

--- free_books/context_processors.py
+++ free_books/context_processors.py
@@ -1,3 +1,5 @@
+from django.utils.html import mark_safe
+
 from .util import website_name
 from .permissions import has_perm
 from .settings import DEBUG
@@ -7,5 +9,7 @@ def common_variables(request):
         # 'debug': True,
         'show_admin': has_perm(request.user, 'admin_view'),
         'show_new_article': has_perm(request.user, 'article_new'),
-        'website_name': website_name()
+        'website_name': website_name(),
+        'downvote_sign': mark_safe('&#x25BC;'),
+        'upvote_sign': mark_safe('&#x25B2;'),
     }
